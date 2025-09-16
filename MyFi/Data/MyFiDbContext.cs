f3fcd886f78a25e409895da7805a60a0a5b067a9
Ab0213ab <aschaefer@my365.bellevue.edu>
2025-09-16 13:43:02 -0400

    Added EFCore, DbContext, mirgations, User model

---
+++ MyFi/Data/MyFiDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFi.Models;
+
+public class MyFiDbContext : DbContext
+{
+    public MyFiDbContext(DbContextOptions<MyFiDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
---
+++ MyFi/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace MyFi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = default!;
+        public string PasswordHash { get; set; } = default!;
+    }
+
+}
