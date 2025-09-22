318c2a5a6613b357661b78403f4990dfe4f2aafc
Ab0213ab <aschaefer@my365.bellevue.edu>
2025-09-22 12:42:48 -0400

    Connect razor pages to new privacy page

---
+++ MyFi/Pages/PrivacyIndex.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFi.Pages
+{
+    public class PrivacyIndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
