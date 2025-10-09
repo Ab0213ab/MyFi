e48166c79be03277dc8138917bb41a1e4af88f74
Ab0213ab <aschaefer@my365.bellevue.edu>
2025-10-09 12:49:10 -0400

    Small column data type change. Decorate column. Remove identity models from migration.

--- MyFi/Models/Expense.cs
+++ MyFi/Models/Expense.cs
@@ -1,11 +1,15 @@
-﻿namespace MyFi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyFi.Models
 {
     public class Expense
     {
         public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
-        public string Amount { get; set; } = default!;
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Amount { get; set; } = default!;
     }
 
 }
